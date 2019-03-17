50449d01cedd3cefe06df23f9d207abdfaad3d52
Adam <ithinkiamadam@gmail.com>
2019-03-16 20:18:07 -0400

    Fix database #3
    DON'T remove critical code from the files.

--- AlexaDB.py
+++ AlexaDB.py
@@ -3,7 +3,8 @@ import json
 from Main import addItemFromDatabase
 
 def updateDataFromDatabase():
-	dynamodb=boto3.resource('dynamodb',aws_access_key_id='AKIAJ2G7XDVUD22HN3RA',aws_secret_access_key='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',region_name='us-east-1')    table = dynamodb.Table('blairfinalfinaltable')
+	dynamodb=boto3.resource('dynamodb',aws_access_key_id='AKIAJ2G7XDVUD22HN3RA',aws_secret_access_key='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',region_name='us-east-1')
+    table = dynamodb.Table('blairfinalfinaltable')
     data = table.scan()["Items"]
     for task in data:
 	    addItemFromDatabase(task["activityName"], task["duration"])
